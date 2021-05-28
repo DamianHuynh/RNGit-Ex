0c083131c868235997653b1cf35b48da2aa4df5e
Huynh Tuan Dat <dat.huynh@DatHuynhODSs-MacBook-Pro.local>
2021-05-28 22:01:49 +0700

    Implement UI get data from API

--- src/components/Lifecycle/BaiTapLifecycle.js
+++ src/components/Lifecycle/BaiTapLifecycle.js
@@ -1,37 +1,76 @@
 import React, {Component} from 'react';
-import {Image, Text, View, ActivityIndicator} from 'react-native';
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  FlatList,
+} from 'react-native';
 import axios from 'axios';
+import ProductItem from './ProductItem';
 
 export default class BaiTapLifecycle extends Component {
   state = {
-    movie: {},
-    loading: false,
+    product: {},
+    listProduct: [],
   };
 
   componentDidMount() {
-    this.setState({loading: true});
     axios({
       method: 'GET',
-      url:
-        'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP12',
+      url: 'http://svcy3.myclass.vn/api/Product',
     })
-      .then(res => console.log(res))
+      .then(res =>
+        this.setState({
+          product: res.data.content[0],
+          listProduct: res.data.content,
+        }),
+      )
       .catch(error => console.log(error));
   }
 
+  _renderItem = ({item}) => {
+    return <ProductItem product={item} />;
+  };
+
   render() {
-    console.log(this.state.movie);
+    // console.log(this.state.product);
+
     return (
-      <View style={{marginTop: 50}}>
-        <ActivityIndicator animating={this.state.loading} size="large" />
-        <Image
-          source={{uri: this.state.movie.hinhAnh}}
-          style={{height: 200, width: 150, backgroundColor: 'gray'}}
-        />
-        <Text> {this.state.movie.tenPhim} </Text>
-        <Text> {this.state.movie.moTa} </Text>
-        <Text>{this.state.movie.ngayKhoiChieu}</Text>
-      </View>
+      <SafeAreaView style={styles.areaView}>
+        <View style={styles.container}>
+          {/* <ProductItem product={this.state.product} /> */}
+          <FlatList
+            contentContainerStyle={styles.listProduct}
+            // style={styles.listProduct}
+            horizontal={false}
+            numColumns={2}
+            data={this.state.listProduct}
+            keyExtractor={(item, index) => `${item.name}_${index}`}
+            renderItem={this._renderItem}
+            // ItemSeparatorComponent={() => <View style={{height: 20}} />}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  areaView: {
+    flex: 1,
+    backgroundColor: '#dcf1f9',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  listProduct: {
+    // flex: 1,
+    // backgroundColor: 'red',
+    // justifyContent: 'space-around',
+    // alignItems: 'flex-end',
+  },
+});
